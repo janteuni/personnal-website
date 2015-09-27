1f3fed01a535bce78e2bef530064727d8f9e61c0
janteuni <janteuni@student.42.fr>
2015-09-27 22:51:33 +0200

    feat(anim) add gsap timeline

--- components/Modal.js
+++ components/Modal.js
@@ -8,6 +8,16 @@ export default class Modal extends React.Component {
 
   componentDidMount () {
     document.body.style.overflowY = 'hidden';
+    const overlay = React.findDOMNode(this.refs.overlay);
+    const close = React.findDOMNode(this.refs.close);
+    const modal = React.findDOMNode(this.refs.modal);
+    new TimelineMax()
+      .set([overlay, close, modal], { opacity: 0 })
+      .set(modal, { y: 20 })
+      .set(close, { x: 20 })
+      .to(overlay, 0.2, { opacity: 1 })
+      .to(modal, 0.25, { opacity: 1, y: 0 })
+      .to(close, 0.25, { opacity: 1, x: 0 });
   }
 
   componentDidUnmount () {
@@ -25,9 +35,9 @@ export default class Modal extends React.Component {
 
   render () {
     return (
-      <div className='Modal-overlay' onClick={::this.handleClose}>
-        <i className='ion-close' onClick={::this.handleClose} />
-        <div className='Modal' onClick={::this.stopPropagation}>
+      <div className='Modal-overlay' onClick={::this.handleClose} ref='overlay'>
+        <i className='ion-close' onClick={::this.handleClose} ref='close' />
+        <div className='Modal' onClick={::this.stopPropagation} ref='modal'>
           {this.props.component}
         </div>
       </div>
