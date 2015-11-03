d1339f7cce9b02289f972e48c444d5a9f78b251e
janteuni <janteuni@student.42.fr>
2015-11-03 22:47:08 +0100

    [CORE] rename images

--- components/modals/PlantsModal.js
+++ components/modals/PlantsModal.js
@@ -11,29 +11,29 @@ export default class PlantsModal extends React.Component {
       <div className='PlantsModal'>
         <div className='row row-end sm-wrap'>
           <div className='col-def'>
-            <img src='images/plantes/1.jpg' />
+            <img src='images/plantes/jasmine-anteunis-plants-1.jpg' alt='Jasmine Anteunis - collection of plants'/>
           </div>
           <div className='col-def'>
-            <img src='images/plantes/2.jpg' />
+            <img src='images/plantes/jasmine-anteunis-cactus-2.jpg' alt='Jasmine Anteunis - Cactus and water'/>
           </div>
         </div>
         <div className='row'>
           <div className='col'>
-            <img src='images/plantes/3.jpg' />
+            <img src='images/plantes/jasmine-anteunis-plants-3.jpg' alt='Jasmine Anteunis - First Ficus'/>
           </div>
           <div className='col'>
-            <img src='images/plantes/4.jpg' />
+            <img src='images/plantes/jasmine-anteunis-cactus-4.jpg' alt='Jasmine Anteunis - Jardin acclimatation Paris cactus'/>
           </div>
           <div className='col'>
-            <img src='images/plantes/5.jpg' />
+            <img src='images/plantes/jasmine-anteunis-plants-5.jpg' alt='Jasmine Anteunis - Beautiful Cactus and Plants'/>
           </div>
         </div>
         <div className='row'>
           <div className='col-def'>
-            <img src='images/plantes/6.jpg' />
+            <img src='images/plantes/jasmine-anteunis-cactus-6.jpg' alt='Jasmine Anteunis - collection of cactus'/>
           </div>
           <div className='col-def'>
-            <img src='images/plantes/7.jpg' />
+            <img src='images/plantes/jasmine-anteunis-cactus-7.jpg' alt='Jasmine Anteunis - When my cactus meet dinosaurs' />
           </div>
         </div>
       </div>
